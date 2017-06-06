46e9341e0e277bcb1e9f254dc1d52e3ee8a40e8a
yowko <yowko0729@gmail.com>
2017-06-06 18:26:43 +0800

    Add project files.

---
+++ TDD-Day2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDD-Day2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("TDD-Day2")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90844de3-4876-4f20-9e78-192ada610947")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDD-Day2/UnitTest_Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TDD_Day2
+{
+    [TestClass]
+    public class UnitTest_Cart
+    {
+        public void 第一集買了一本_其他都沒買_價格應為100元()
+        {
+            //arrange
+            var shoppingItems = new List<Book>()
+            {
+                new Book {Id=1,Price=100,Name="Harry Potter 1" }
+            };
+            var target = new Cart(rules);
+
+
+
+            var expected = 100;
+            //act
+            var actual = target.Checkout(shoppingItems, a => a.Price);
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
