b4b52bbed30cb8f98f61f09191edd60f65c601a0
yowko <yowko0729@gmail.com>
2017-06-06 19:16:24 +0800

    Scenario fail

--- TDD-Day2/UnitTest_Cart.cs
+++ TDD-Day2/UnitTest_Cart.cs
@@ -131,5 +131,28 @@ namespace TDD_Day2
             //assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void 一二集各買了一本_第三集買了兩本_價格應為370()
+        {
+            //arrange
+            var shoppingItems = new List<Book>()
+            {
+                new Book {Id=1,Price=100,Name="Harry Potter 1" },
+                new Book {Id=2,Price=100,Name="Harry Potter 2" },
+                new Book {Id=3,Price=100,Name="Harry Potter 3" },
+                new Book {Id=3,Price=100,Name="Harry Potter 3" }
+            };
+            var target = new Cart(rules);
+
+
+
+            var expected = 370;
+            //act
+            var actual = target.Checkout(shoppingItems, a => a.Price);
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
